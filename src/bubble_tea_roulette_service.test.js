b86fa9255a580483992cc949fb260ac22b2ac4a8
Gurunath <bhuvi23389@gmail.com>
2022-03-10 10:54:05 +0000

    Activity 4: Added test - getRandomBubbleTeaType() function is stubbed with different random value and matches the expected values

--- src/bubble_tea_roulette_service.test.js
+++ src/bubble_tea_roulette_service.test.js
@@ -4,16 +4,16 @@ test.each`
   mockReturnType | expectedBubbleTea
   ${0.2} | ${'JASMINEMILKTEA'}
   ${0.6} | ${'PEACHICETEA'}
-`("should generate '$expectedBubbleTea' bubble tea when the ramdom value is '$mockReturnType'", ({mockReturnType, $expectedBubbleTea}) => {
+`("should generate '$expectedBubbleTea' bubble tea when the ramdom value is '$mockReturnType'", ({mockReturnType, expectedBubbleTea}) => {
   // Arrange
   // Stub to calls to Math.random to return 0.2
-  jest.spyOn(global.Math, 'random').mockReturnValue(0.2);
+  jest.spyOn(global.Math, 'random').mockReturnValue(mockReturnType);
 
   // Act
   const bubbleTea = getRandomBubbleTeaType();
 
   // Assert
-  expect(bubbleTea).toBe('JASMINEMILKTEA');
+  expect(bubbleTea).toBe(expectedBubbleTea);
 
   // Restore the default Math.random
   jest.spyOn(global.Math, 'random').mockRestore();
